85e23deaa63ac72317ab9866b928b83c6272adc6
KrazyLama789 <maryrgoff@gmail.com>
2024-12-11 21:35:52 -0700

    Update: derefrenced the table and cells into their own nibs.

---
+++ src/calendar/cell.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Cell() {
+    return (
+        <td className="clickable" onclick=""></td>
+    );
+}
---
+++ src/calendar/table.jsx
@@ -0,0 +1,516 @@
+import React from "react";
+import { Cell } from "./cell"
+
+export function Table() {
+    return (
+        <table className="table table-hover table-bordered">
+            <thead>
+                <tr id="cal-headings">
+                    <th></th>
+                    <th>Sunday <br></br><span id="sunday-date">1st</span></th>
+                    <th>Monday <br></br><span id="monday-date">2nd</span></th>
+                    <th>Tuesday <br></br><span id="tuesday-date">3rd</span></th>
+                    <th>Wednesday <br></br><span id="wednesday-date">4th</span></th>
+                    <th>Thursday <br></br><span id="thursday-date">5th</span></th>
+                    <th>Friday <br></br><span id="friday-date">6th</span></th>
+                    <th>Saturday <br></br><span id="saturday-date">7th</span></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>12:00 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>12:30 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>1:00 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>1:30 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>2:00 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>2:30 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+
+                <tr>
+                    <th>3:00 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>3:30 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+
+                <tr>
+                    <th>4:00 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>4:30 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+
+                <tr>
+                    <th>5:00 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>5:30 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>6:00 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>6:30 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+
+                <tr>
+                    <th>7:00 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>7:30 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>8:00 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>8:30 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>9:00 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>9:30 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+
+                <tr>
+                    <th>10:00 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>10:30 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>11:00 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>11:30 am</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+
+
+                <tr>
+                    <th>12:00 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>12:30 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>1:00 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>1:30 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>2:00 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>2:30 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+
+                <tr>
+                    <th>3:00 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>3:30 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+
+                <tr>
+                    <th>4:00 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>4:30 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+
+                <tr>
+                    <th>5:00 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>5:30 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>6:00 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>6:30 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+
+                <tr>
+                    <th>7:00 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>7:30 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>8:00 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>8:30 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>9:00 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>9:30 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+
+                <tr>
+                    <th>10:00 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>10:30 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>11:00 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+                <tr>
+                    <th>11:30 pm</th>
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                    <Cell />
+                </tr>
+            </tbody>
+
+        </table>
+    );
+}
