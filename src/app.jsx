03863a02bb90d9ec88ba6a7a5a12682c921a48b7
KrazyLama789 <maryrgoff@gmail.com>
2024-12-11 17:41:34 -0700

    removed: misc outdated content

--- src/app.jsx
+++ src/app.jsx
@@ -13,14 +13,14 @@ export default function App() {
         <BrowserRouter>
             <div className='body'>
                 <header>
-                    <nav class="navbar navbar-expand-lg border-bottom border-body">
-                        <div class="container-fluid">
-                            <a class="navbar-brand" href="#">BlockOutCal</a>
-                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
+                    <nav className="navbar navbar-expand-lg border-bottom border-body">
+                        <div className="container-fluid">
+                            <a className="navbar-brand" href="#">BlockOutCal</a>
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
                             </button>
-                            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                                <div class="navbar-nav">
+                            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                                <div className="navbar-nav">
                                     <NavLink className='nav-link' to=''>
                                         Home
                                     </NavLink>
@@ -43,12 +43,12 @@ export default function App() {
                     <Route path='*' element={<NotFound />} />
                 </Routes>
 
-                <footer class="navbar navbar-expand-lg border-top border-body">
-                    <div class="container-fluid">
+                <footer className="navbar navbar-expand-lg border-top border-body">
+                    <div className="container-fluid">
                         <div className="text-reset">
                             Author: Mary G.
                         </div>
-                        <div class="nav-link">
+                        <div className="nav-link">
                             <a href="https://github.com/KrazyLama789/startup.git">Source</a> <a href="">Attribution</a>
                         </div>
                     </div>
